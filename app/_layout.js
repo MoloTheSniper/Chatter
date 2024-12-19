d7ab88756c8abf0324ae71066b96b91a246b2b95
MoloTheSniper <85521815+MoloTheSniper@users.noreply.github.com>
2024-12-19 18:09:06 +0200

    Added Home Page

--- app/_layout.js
+++ app/_layout.js
@@ -1,8 +1,15 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { Slot } from "expo-router";
-
 // Import your global CSS file
 import "../global.css";
 
-export default Slot;
+const _layout = () => {
+  return (
+    <View>
+      <Slot/>
+    </View>
+  )
+}
+
+export default _layout
