288675bac1f64b556df779dbd1d7b1889077649e
MoloTheSniper <85521815+MoloTheSniper@users.noreply.github.com>
2025-01-02 13:56:11 +0200

    Chat UI design

--- app/(app)/chatRoom.tsx
+++ app/(app)/chatRoom.tsx
@@ -1,14 +1,41 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { useLocalSearchParams } from 'expo-router'
-
+import { View, Text, TextInput, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
+import { useLocalSearchParams, useRouter } from 'expo-router'
+import ChatRoomHeader from '@/components/ChatRoomHeader';
+import { StatusBar } from 'expo-status-bar';
+import MessagesList from '@/components/MessagesList';
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen'
+import { Feather } from '@expo/vector-icons';
+import CustomKeyBoardView from '@/components/CustomKeyBoardView';
 const ChatRoom = () => {
     const item = useLocalSearchParams();
-    console.log('got item data: ',item)
+    const router = useRouter();
+    const [messages, setMessages] = useState([]);
   return (
-    <View>
-      <Text>ChatRoom</Text>
-    </View>
+    <CustomKeyBoardView inChat={true}>
+        <View className='flex-1 bg-white'>
+        <StatusBar style="dark"/>  
+        <ChatRoomHeader user = {item} router = {router}/>
+        <View className='h-3 border-b border-neutral-200'/>
+        <View className='flex-1 justifyBetween bg-neutral-100 overflow-visible'>
+            <View className='flex-1'>
+                <MessagesList messages ={messages}/>
+            </View>
+            <View style = {{marginBottom: hp(2.7)}} className='pt-2'>
+                    <View className='flex-row mx-3 justify-between bg-white border p-2 border-neutral-300 rounded-full pl-5'>
+                        <TextInput
+                            placeholder='Type message...'
+                            style ={{fontSize: hp(2)}}
+                            className='flex-1 mr-2'
+                        />
+                        <TouchableOpacity className='bg-neutral-200 p-2 mr-[1px] rounded-full'>
+                        <Feather name='send' size={hp(2.7)} color={'#737373'}  />
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </View>
+        </View>
+      </CustomKeyBoardView>
   )
 }
 
--- components/CustomKeyBoardView.tsx
+++ components/CustomKeyBoardView.tsx
@@ -2,15 +2,26 @@ import { View, Text, KeyboardAvoidingView, ScrollView, Platform } from 'react-na
 import React from 'react'
 
 const ios = Platform.OS == 'ios';
-const CustomKeyBoardView = ({children}) => {
+const CustomKeyBoardView = ({children, inChat}) => {
+  let kavConfig = {};
+  let scrollViewConfig = {};
+  if(inChat)
+    {
+      kavConfig = {keyboardVerticalOffset: 90};
+      scrollViewConfig = {contentContainerStyle: {flex: 1}};
+    }
   return (
     <KeyboardAvoidingView
         behavior = {ios? 'padding':'height'}
-        style={{flex: 1}}>
+        style={{flex: 1}}
+        {...kavConfig}
+        >
+         
         <ScrollView
             style ={{flex: 1}}
             bounces = {false}
             showsVerticalScrollIndicator= {false}
+            {...scrollViewConfig}
         >
             {
                 children
