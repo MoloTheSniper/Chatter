dccd6d454455cb7de24ba01474ea5d17923416c0
MoloTheSniper <85521815+MoloTheSniper@users.noreply.github.com>
2025-01-03 16:06:09 +0200

    Chat functionality added

--- app/(app)/chatRoom.tsx
+++ app/(app)/chatRoom.tsx
@@ -1,5 +1,5 @@
-import { View, Text, TextInput, TouchableOpacity } from 'react-native'
-import React, { useState } from 'react'
+import { View, Text, TextInput, TouchableOpacity, Alert } from 'react-native'
+import React, { useEffect, useRef, useState } from 'react'
 import { useLocalSearchParams, useRouter } from 'expo-router'
 import ChatRoomHeader from '@/components/ChatRoomHeader';
 import { StatusBar } from 'expo-status-bar';
@@ -7,10 +7,67 @@ import MessagesList from '@/components/MessagesList';
 import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen'
 import { Feather } from '@expo/vector-icons';
 import CustomKeyBoardView from '@/components/CustomKeyBoardView';
+import { useAuth } from '../context/authContext';
+import { getRoomId } from '../utils/common';
+import { Timestamp, addDoc, collection, doc, onSnapshot, orderBy, query, setDoc } from 'firebase/firestore';
+import { db } from '@/firebaseConfig';
 const ChatRoom = () => {
-    const item = useLocalSearchParams();
+    const item = useLocalSearchParams(); //second user
+    const {user} = useAuth(); //Logged in user
     const router = useRouter();
     const [messages, setMessages] = useState([]);
+    const textRef = useRef('');
+    const inputRef = useRef(null);
+
+    useEffect(() =>{
+        createRoomIfNotExists();
+
+        let roomId = getRoomId(user?.userId, item?.userId);
+        const docRef = doc(db, "rooms", roomId);
+        const messagesRef = collection(docRef, "messages");
+        const q = query(messagesRef, orderBy('createdAt','asc'))
+
+        let unsub = onSnapshot(q, (snapshot)=>{
+            let allMessages = snapshot.docs.map(doc =>{
+                return doc.data(); 
+            })
+            setMessages([...allMessages]);
+        })
+        return unsub;
+    },[])
+
+    const createRoomIfNotExists = async () =>
+    {
+        let roomId = getRoomId(user?.userId, item?.userId);
+        await setDoc(doc(db,"rooms", roomId),{
+            roomId,
+            createdAt: Timestamp.fromDate(new Date())
+        });
+    }
+    const handleSendMessage = async () =>{
+        let message = textRef.current.trim();
+        if(!message) return;
+        try{
+            let roomId = getRoomId(user?.userId, item?.userId)
+            const docRef = doc(db, 'rooms', roomId);
+            const messagesRef = collection(docRef, "messages");
+            textRef.current = "";
+            if(inputRef) inputRef?.current?.clear();
+            const newDoc = await addDoc(messagesRef,{
+                userId: user?.userId,
+                text: message,
+                profileUrl: user?.profileUrl,
+                senderName: user?.username,
+                createdAt: Timestamp.fromDate(new Date())
+            })
+
+            console.log('new message id: ', newDoc.id)
+        }catch(err)
+        {
+            Alert.alert('Message', err.message);
+        }
+    }
+
   return (
     <CustomKeyBoardView inChat={true}>
         <View className='flex-1 bg-white'>
@@ -19,17 +76,19 @@ const ChatRoom = () => {
         <View className='h-3 border-b border-neutral-200'/>
         <View className='flex-1 justifyBetween bg-neutral-100 overflow-visible'>
             <View className='flex-1'>
-                <MessagesList messages ={messages}/>
+                <MessagesList messages ={messages} currentUser={user}/>
             </View>
             <View style = {{marginBottom: hp(2.7)}} className='pt-2'>
                     <View className='flex-row mx-3 justify-between bg-white border p-2 border-neutral-300 rounded-full pl-5'>
                         <TextInput
+                            ref = {inputRef}
+                            onChangeText={value => textRef.current = value}
                             placeholder='Type message...'
                             style ={{fontSize: hp(2)}}
                             className='flex-1 mr-2'
                         />
-                        <TouchableOpacity className='bg-neutral-200 p-2 mr-[1px] rounded-full'>
-                        <Feather name='send' size={hp(2.7)} color={'#737373'}  />
+                        <TouchableOpacity onPress ={handleSendMessage} className='bg-neutral-200 p-2 mr-[1px] rounded-full'>
+                            <Feather name='send' size={hp(2.7)} color={'#737373'}  />
                         </TouchableOpacity>
                     </View>
                 </View>
